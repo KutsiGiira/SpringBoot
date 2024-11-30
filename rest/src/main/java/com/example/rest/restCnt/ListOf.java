637ee3765d4b735d6d6495ec9b9720ae65652cbd
KutsiGira <haytamboualkhours@gmail.com>
2024-11-30 16:49:07 +0100

    building a REST API (search by index)

--- rest/src/main/java/com/example/rest/restCnt/ListOf.java
+++ rest/src/main/java/com/example/rest/restCnt/ListOf.java
@@ -1,20 +1,29 @@
 package com.example.rest.restCnt;
 
 import com.example.rest.entity.Students;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 @RestController
 @RequestMapping
 public class ListOf {
-    @GetMapping("/list")
-    public List<Students> List(){
-        List<Students> l = new ArrayList<>();
+    private List<Students> l;
+    @PostConstruct
+    public void  Data(){
+        l = new ArrayList<>();
         l.add(new Students("Black", "Kutsi"));
         l.add(new Students("KutsiGria", "Blake"));
+    }
+    @GetMapping("/list")
+    public List<Students> List(){
         return l;
     }
+    @GetMapping("/list/{id}")
+    public Students getName(@PathVariable int id){
+        return l.get(id);
+    }
+
 }
