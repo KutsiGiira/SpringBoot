4f706e213360c1ec633ac800cdda60f72d058252
KutsiGira <haytamboualkhours@gmail.com>
2024-12-02 19:18:21 +0100

    REST + CRUD (+ JPA Repository)

---
+++ RESTCRUD (With JPA)/src/main/java/com/example/RESTCRUD/service/ServiceDao.java
@@ -0,0 +1,45 @@
+package com.example.RESTCRUD.service;
+
+import com.example.RESTCRUD.Dao.MoviesRepo;
+import com.example.RESTCRUD.Entity.movies;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceDao implements ServiceInt{
+    private MoviesRepo movi;
+@Autowired
+    public ServiceDao(MoviesRepo  movi) { //CAUTION
+        this.movi = movi;
+    }
+
+    @Override
+    public List<movies> show() {
+        return movi.findAll();
+    }
+
+    @Override
+    public movies findById(int id) {
+        Optional<movies> r = movi.findById(id);
+        movies m = null;
+        if(r.isPresent()){
+            m = r.get();
+        }
+        else{
+            throw new RuntimeException("Did not be found");
+        }
+        return m;
+    }
+    @Override
+    public void delete(int id) {
+        movi.deleteById(id);
+    }
+    @Override
+    public movies save(movies mov) {
+        return movi.save(mov);
+    }
+}
