99543fe3194b3299fafe20de7cf5cbc696bd8b50
KutsiGiira <haytamboualkhours@gmail.com>
2024-12-30 22:10:49 +0100

    Git init (Building quote gen)

---
+++ QuotesGen/src/main/java/com/example/QuotesGen/QuotesGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuotesGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesGenApplication.class, args);
+	}
+
+}
---
+++ QuotesGen/src/test/java/com/example/QuotesGen/QuotesGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuotesGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
