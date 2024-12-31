2cbb4eaca6af7f0d89c22cdee9c199b4dee62bad
KutsiGiira <haytamboualkhours@gmail.com>
2024-12-31 21:49:55 +0100

    Diving into Security dependencies

--- Securityv2/src/main/java/com/example/Securityv2/Config/Config.java
+++ Securityv2/src/main/java/com/example/Securityv2/Config/Config.java
@@ -42,6 +42,7 @@ public class Config {
                         .requestMatchers("/admin/**").hasRole("admin")
                         .anyRequest().authenticated()
         )
+
                 .formLogin(form ->
                         form.loginPage("/myLogin").loginProcessingUrl("/logged").permitAll()
                         )
